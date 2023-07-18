45f5a179a5e20c49b553b27574393aad09d15424
ASTRÆUS <0x00ASTRA@proton.me>
2023-07-18 09:34:27 -0500

    added a filesystem feaature

---
+++ tests/filesystem_test.py
@@ -0,0 +1,15 @@
+import sys
+import os
+
+# Get the absolute path of the project's root directory
+root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Add the root directory to the Python path
+sys.path.append(root_path)
+
+from classes.filesystem import Filesystem
+
+f1 = Filesystem('/')
+f1.add_file('/', 'hello.txt')
+f1.add_folder('/', 'Docs')
+print(f1.contains)
