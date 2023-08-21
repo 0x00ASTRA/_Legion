3e1498856308bdbfe41128eb03c210957deb4940
ajblondell <ajblondell@protonmail.com>
2023-08-21 11:50:55 -0500

    improved modularity of generate_serial_id()

--- classes/utils.py
+++ classes/utils.py
@@ -1,10 +1,11 @@
 import random
-def generate_serial_id():
+
+def generate_serial_id(id_length = 6, section_size = 4):
     id = ''
     chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
-    id_length = 10
+
     for i in range(id_length):
-        id += random.choice(chars)*2
-        if i <10:
+        id += random.choice(chars) * section_size
+        if i < id_length:
             id += '::'
     return id
