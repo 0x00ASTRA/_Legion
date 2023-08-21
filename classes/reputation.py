777d7349c1fab935ad93300e9d5976ee194b0c9c
ajblondell <ajblondell@protonmail.com>
2023-08-21 11:41:41 -0500

    redesigned the reputation system architecture

--- classes/reputation.py
+++ classes/reputation.py
@@ -1,15 +1,30 @@
+from typing import Dict
+import json
+
 class Reputation:
     def __init__(self):
-        self.reputation_points = 0
+        self.keypairs: Dict[str, Dict] = {
+            'total': {'points': 0},
+            'hacking': {'points': 0},
+            'company': {'points': 0},
+            'darknet': {'points': 0},
+            'clearnet': {'points': 0},
+            'commerce': {'points': 0},
+            'organization': {'name': '', 'points': 0}
+        }
 
-    def increment_reputation(self, points):
-        self.reputation_points += points
+    def increment_reputation(self, key: str, target: str, points: float) -> None:
+        if key in self.keypairs:
+            if target not in self.keypairs[key]:
+                self.keypairs[key][target] = {'points': 0}
+            self.keypairs[key][target]['points'] += points
 
-    def decrement_reputation(self, points):
-        self.reputation_points -= points
+    def decrement_reputation(self, key: str, target: str, points: float) -> None:
+        if key in self.keypairs and target in self.keypairs[key]:
+            self.keypairs[key][target]['points'] -= points
 
-    def get_reputation(self):
-        return self.reputation_points
+    def get_reputation(self, key: str, target: str) -> float:
+        return self.keypairs.get(key, {}).get(target, {}).get('points', 0)
 
-    def to_json(self):
-        return self.reputation_points
+    def to_json(self) -> str:
+        return json.dumps(self.keypairs)
