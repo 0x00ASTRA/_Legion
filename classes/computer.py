fc65676d4d081954e0c39c3d05a4d9ef11ced4ce
ajblondell <ajblondell@protonmail.com>
2023-11-01 14:26:48 -0500

    introduced file management (unfinished)

--- classes/computer.py
+++ classes/computer.py
@@ -1,4 +1,5 @@
 import classes.utils as utils
+from classes.filesystem import Filesystem
 class Computer:
     def __init__(self, CPU, GPU, RAM, storage):
         self.CPU = CPU
@@ -7,3 +8,7 @@ class Computer:
         self.storage = storage
         self.is_compromised = False
         self.serial_id = utils.generate_serial_id()
+        self.filesystem = Filesystem('/')
+
+    def list_files(self) -> str:
+        return str(self.filesystem.contains)
