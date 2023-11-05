3fc6b4f8e4e86017f8912c7f0e72f1ef8c4585c1
ajblondell <ajblondell@protonmail.com>
2023-11-05 15:15:53 -0600

    now uses crypto currency for lookup

--- classes/crypto/crypto_market.py
+++ classes/crypto/crypto_market.py
@@ -1,10 +1,11 @@
 import random
+import classes.crypto.crypto_currency as CryptoCurrency
 
 class CryptoMarket:
     def __init__(self, prices):
-        self.prices = prices
+        self.currencies: CryptoCurrency = []
 
     def update_prices(self):
         for currency in self.prices:
             # The price of each currency fluctuates randomly to simulate demand and supply
-            self.prices[currency] *= random.uniform(0.9, 1.1)
+            self.currencies[currency].price *= random.uniform(0.9, 1.1)
