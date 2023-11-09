cec36fd3f5fc3d902bced31ceebcf1a4835e2c82
ASTRÆUS <ajblondell@protonmail.com>
2023-11-09 02:30:28 -0600

    initial XP system code

---
+++ classes/experience/XPStatCategories/xp_stat_category.py
@@ -0,0 +1,3 @@
+class XPStatCategory:
+    def __init__(self):
+        pass
---
+++ classes/experience/experience_system.py
@@ -0,0 +1,18 @@
+from classes.experience.XPCategories.it import IT
+#
+# (!) Incomplete (!)
+#
+class ExperienceSystem:
+    def __init__(
+        self,
+        it: IT,
+        tactical: Tactical,
+        social: Social,
+        physical: Physical,
+        organization: Organization
+    ):
+        self.it = it
+        self.tactical = tacical
+        self.social = social
+        self.physical = physical
+        organization = organization
